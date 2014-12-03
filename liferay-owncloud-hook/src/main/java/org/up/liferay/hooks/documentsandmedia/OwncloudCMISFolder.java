994b8f1690323bbf8fc96ab756371dec873f5ae4
uzuzjmd <julian.dehne@gmail.com>
2014-12-03 11:18:12 +0100

    integrated fix for get parent LPS-51879

---
+++ liferay-owncloud-hook/src/main/java/org/up/liferay/hooks/documentsandmedia/OwncloudCMISFolder.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package org.up.liferay.hooks.documentsandmedia;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.lar.StagedModelType;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.util.CharPool;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.model.User;
+import com.liferay.portal.repository.cmis.model.CMISFolder;
+import com.liferay.portal.security.permission.ActionKeys;
+import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.CMISRepositoryLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.model.DLFolder;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
+import com.liferay.portlet.documentlibrary.service.DLAppLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.DLFolderLocalServiceUtil;
+import com.liferay.portlet.documentlibrary.service.permission.DLFolderPermission;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.chemistry.opencmis.client.api.CmisObject;
+import org.apache.chemistry.opencmis.client.api.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Julian Dehne
+ */
+public class OwncloudCMISFolder extends CMISFolder {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static final Logger log = LoggerFactory.getLogger(OwncloudCMISFolder.class
+			.getName());
+	private org.apache.chemistry.opencmis.client.api.Folder _cmisFolder;
+
+	public OwncloudCMISFolder(OwncloudCMISRepository cmisRepository, String uuid,
+			long folderId,
+			org.apache.chemistry.opencmis.client.api.Folder cmisFolder) {		
+		super(cmisRepository, uuid, folderId, cmisFolder);
+		this._cmisFolder = cmisFolder; //cannot change visibility of _cmisFolder field
+	}
+	
+		
+	
+
+		
+	
+
+	@Override
+	public Folder getParentFolder() throws PortalException, SystemException {
+		Folder parentFolder = null;
+
+		try {
+			parentFolder = super.getParentFolder();
+
+			if (parentFolder != null) {
+				return parentFolder;
+			}
+		} catch (Exception e) {
+		}
+
+		if (_cmisFolder.isRootFolder()) {
+			parentFolder = returnRootFolder();
+			return parentFolder;
+		} else {
+			String path = _cmisFolder.getPath();
+						
+			if (path.endsWith("/")) {
+				log.trace("changed computation of parent path to assume / in the end of folders [Julian-UP]");
+				String tmp = path.substring(0, path.lastIndexOf("/"));
+				path = path.substring(0, tmp.lastIndexOf("/") + 1);
+
+				if (path.length() == 0) {
+					path = StringPool.SLASH;
+				}
+
+				log.trace("changed computation of parent path to assume / to be root folder path [Julian-UP]");
+				if (path.equals("/")) {
+					parentFolder = returnRootFolder();
+					return parentFolder;
+				}
+				
+				log.warn("using fix for LPS-51879");
+				
+			} else {
+				path = path.substring(0, path.lastIndexOf(CharPool.SLASH));
+			}
+
+			Session session = (Session) CMISRepositoryLocalServiceUtil
+					.getSession(getRepositoryId());
+
+			CmisObject parentCmisFolder = session.getObjectByPath(path);
+
+			parentFolder = CMISRepositoryLocalServiceUtil.toFolder(
+					getRepositoryId(), parentCmisFolder);
+		}
+
+		setParentFolder(parentFolder);
+
+		return parentFolder;
+	}
+
+	private Folder returnRootFolder() throws PortalException, SystemException {
+		Folder parentFolder;
+		Folder folder = DLAppLocalServiceUtil.getMountFolder(getRepositoryId());
+
+		parentFolder = folder.getParentFolder();
+		return parentFolder;
+	}
+	
+	
+}
--- liferay-owncloud-hook/src/main/java/org/up/liferay/hooks/documentsandmedia/OwncloudCMISRepository.java
+++ liferay-owncloud-hook/src/main/java/org/up/liferay/hooks/documentsandmedia/OwncloudCMISRepository.java
@@ -14,15 +14,22 @@
 
 package org.up.liferay.hooks.documentsandmedia;
 
+import java.util.List;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.repository.cmis.CMISRepositoryHandler;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.repository.cmis.CMISRepository;
+import com.liferay.portal.repository.cmis.model.CMISFolder;
 
 
 /**
  * CMIS does not provide vendor neutral support for workflow, metadata, tags,
  * categories, etc. They will be ignored in this implementation.
  *
- * @author Alexander Chow
+ * @author Julian Dehne
  * @see <a href="http://wiki.oasis-open.org/cmis/Candidate%20v2%20topics">
  *      Candidate v2 topics</a>
  * @see <a href="http://wiki.oasis-open.org/cmis/Mixin_Proposal">Mixin / Aspect
@@ -32,15 +39,45 @@ import com.liferay.portal.repository.cmis.CMISRepository;
  *      CMIS Type Mutability proposal</a>
  */
 public class OwncloudCMISRepository extends CMISRepository {
-
-	static OwncloudCMISAtomPubRepository endpoint = new OwncloudCMISAtomPubRepository();
 	
-//	public OwncloudCMISRepository() {
-//		super(endpoint);			
-//		endpoint.setCmisRepository(this);
-//	}
 	
 	public OwncloudCMISRepository(CMISRepositoryHandler cmisRepositoryHandler) {
 		super(cmisRepositoryHandler);
 	}
+	
+	@Override
+	public Folder toFolder(
+			org.apache.chemistry.opencmis.client.api.Folder cmisFolder)
+		throws SystemException {
+
+		Object[] ids = getRepositoryEntryIds(cmisFolder.getId());
+
+		long folderId = (Long)ids[0];
+		String uuid = (String)ids[1];
+
+		return new OwncloudCMISFolder(this, uuid, folderId, cmisFolder);
+	}
+	
+	@Override
+	protected List<Object> getFoldersAndFileEntries(long folderId)
+			throws SystemException {
+		System.err.println("i am hiere");
+		return super.getFoldersAndFileEntries(folderId);
+	}
+	
+	@Override
+	public List<Object> getFoldersAndFileEntries(long folderId, int start,
+			int end, OrderByComparator obc) throws SystemException {
+		System.err.println("i am hiere");
+		return super.getFoldersAndFileEntries(folderId, start, end, obc);
+	}
+	
+	@Override
+	public List<Object> getFoldersAndFileEntries(long folderId,
+			String[] mimeTypes, int start, int end, OrderByComparator obc)
+			throws PortalException, SystemException {
+		System.err.println("i am hiere");
+		return super.getFoldersAndFileEntries(folderId, mimeTypes, start, end, obc);
+	}
+		
 }
